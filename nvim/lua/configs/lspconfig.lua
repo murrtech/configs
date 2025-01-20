1d90506206cf6fd54e91be24fe67b995033df125
Ben Murray <murraybenjamin1@gmail.com>
2025-01-19 23:35:30 -0700

    commit

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,58 +1,24 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
+
 local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls", "rust-analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
-
--- Configure diagnostic settings for real-time updates
-vim.diagnostic.config({
- update_in_insert = true,  -- This enables real-time diagnostics in insert mode
- virtual_text = {
-   prefix = "●",
-   spacing = 4,
-   source = "always",  -- Show source of diagnostic
- },
- float = {
-   source = "always",  -- Show source in floating window
- },
- severity_sort = true,  -- Sort diagnostics by severity
-})
-
--- Add keymaps for LSP features
-vim.api.nvim_create_autocmd('LspAttach', {
- callback = function(ev)
-   local opts = { buffer = ev.buf }
-   vim.keymap.set("n", "<F12>", vim.lsp.buf.definition, opts)
-   vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-   vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
- end,
-})
-
--- Your servers configuration
-local servers = { "html", "cssls" }
-
--- Rust analyzer setup
-lspconfig.rust_analyzer.setup {
- on_attach = nvlsp.on_attach,
- on_init = nvlsp.on_init,
- capabilities = nvlsp.capabilities,
- settings = {
-   ['rust-analyzer'] = {
-     checkOnSave = {
-       command = "clippy"
-     },
-   }
- }
-}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
- lspconfig[lsp].setup {
-   on_attach = nvlsp.on_attach,
-   on_init = nvlsp.on_init,
-   capabilities = nvlsp.capabilities,
-   flags = {
-     debounce_text_changes = 40,  -- Reduce delay before sending changes to LSP
-   },
- }
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
 end
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,7 +3,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -19,31 +19,117 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "lua-language-server", "stylua",
-        "html-lsp", "css-lsp", "prettier",
-        "rust-analyzer" -- Add rust-analyzer for Rust language support
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "rust-analyzer",
+        "typescript-language-server",
+        "emmet-ls",
+        "json-lsp",
+        "shfmt",
+        "shellcheck",
       },
     },
   },
+  { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "html", "css", "rust", "toml", "yaml", "python", "json" -- Add rust parser for syntax highlighting
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "rust",
+        "toml",
+        "yaml",
+        "python",
+        "json", -- Add rust parser for syntax highlighting
       },
     },
   },
+
   {
-    'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
-    lazy = false,   -- This plugin is already lazy
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+
+  {
+    "mhartington/formatter.nvim",
+    lazy = false,
+    config = function()
+      -- Define the formatting logic for various file types
+      require("formatter").setup {
+        filetype = {
+          rust = {
+            -- Use rustfmt for Rust files
+            function()
+              return {
+                exe = "rustfmt",
+                args = { "--emit=stdout" },
+                stdin = true,
+              }
+            end,
+          },
+          lua = {
+            -- Use stylua for Lua files
+            function()
+              return {
+                exe = "stylua",
+                args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "-" },
+                stdin = true,
+              }
+            end,
+          },
+          html = {
+            -- Use prettier for HTML files
+            function()
+              return {
+                exe = "prettier",
+                args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+                stdin = true,
+              }
+            end,
+          },
+          python = {
+            -- Use black for Python files
+            function()
+              return {
+                exe = "black",
+                args = { "-" },
+                stdin = true,
+              }
+            end,
+          },
+          yaml = {
+            -- Use prettier for YAML files
+            function()
+              return {
+                exe = "prettier",
+                args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+                stdin = true,
+              }
+            end,
+          },
+        },
+      }
+
+      -- Auto-format on save
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = { "*.rs", "*.lua", "*.html", "*.py", "*.yaml", "*.yml" },
+        command = "FormatWrite",
+      })
+    end,
   },
   {
-    'saecki/crates.nvim',
-    ft = { 'toml' },
+    "saecki/crates.nvim",
+    ft = { "toml" },
     config = function()
-      require('crates').setup()
+      require("crates").setup()
     end,
   },
 
@@ -60,10 +146,10 @@ return {
         defaults = {
           file_ignore_patterns = {
             "node_modules/.*", -- Ignore all subfolders/files in node_modules
-            "%.git/.*",        -- Ignore all subfolders/files in .git
-            "build/.*",        -- Ignore build folder recursively
-            "target/.*",       -- Ignore all subfolders/files in target
-            ".*%.lock",        -- Ignore lock files like yarn.lock or package-lock.json
+            "%.git/.*", -- Ignore all subfolders/files in .git
+            "build/.*", -- Ignore build folder recursively
+            "target/.*", -- Ignore all subfolders/files in target
+            ".*%.lock", -- Ignore lock files like yarn.lock or package-lock.json
           },
         },
       }
@@ -71,12 +157,9 @@ return {
   },
   {
     "folke/todo-comments.nvim",
+    lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
+    opts = {},
   },
   {
     "hedyhli/outline.nvim",
@@ -86,18 +169,18 @@ return {
       {
         "<leader>o",
         "<cmd>belowright Outline<CR>",
-        desc = "Toggle outline"
-      }
+        desc = "Toggle outline",
+      },
     },
     config = function()
-      require("outline").setup({
+      require("outline").setup {
         outline_window = {
-          position = 'bottom',
+          position = "bottom",
           width = 25,
           auto_jump = true,
-        }
-      })
-    end
+        },
+      }
+    end,
   },
   {
     "folke/trouble.nvim",
@@ -109,31 +192,6 @@ return {
         "<cmd>Trouble diagnostics toggle focus=true<cr>",
         desc = "Diagnostics (Trouble)",
       },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
     },
-  }
+  },
 }
